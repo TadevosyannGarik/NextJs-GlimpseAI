5427857c45dec7b91ef83e5adc0546c58a93abd3
TadevosyannGarik <tadevosyan12@gmail.com>
2024-02-17 15:49:49 +0400

    Layout Sidebar & Mobile Navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,11 @@
+import MobileNavbar from "@/components/shared/mobile-navbar"
+import Sidebar from "@/components/shared/sidebar"
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="root">
+            <Sidebar />
+            <MobileNavbar />
             <div className="root-container">
                 <div className="wrapper">
                     {children}
---
+++ components/shared/mobile-navbar.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
+import { navLinks } from "@/constants"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { Button } from "@/components/ui/button";
+  
+
+const MobileNavbar = () => {
+    const pathname = usePathname();
+
+
+    return (
+        <header className="header">
+            <Link href="/" className="flex items-center gap-2 md:py-2">
+                <p className="text-4xl text-center font-bold bg-gradient-to-r from-indigo-600 via-purple-600 to-purple-900 bg-clip-text text-transparent">
+                    Glipmse AI
+                </p>
+            </Link>
+            <nav className="flex gap-2">
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/" />
+                    <Sheet>
+                        <SheetTrigger>
+                            <Image 
+                                src="/assets/icons/menu.svg"
+                                width={32}
+                                height={32}
+                                alt="menu"
+                                className="cursor-pointer"
+                            />
+                        </SheetTrigger>
+                        <SheetContent className="sheet-content sm:w-64">
+                            <>
+                                <p className="text-4xl text-center font-bold bg-gradient-to-r from-indigo-600 via-purple-600 to-purple-900 bg-clip-text text-transparent">
+                                    Glipmse AI
+                                </p>
+                                <ul className="header-nav_elements">
+                                    {navLinks.map((link) => {
+                                        const isActive = link.route === pathname
+                                        return (
+                                            <li className={`${isActive && 'gradient-text'} p-18 flex whitespace-nowrap text-dark-700`} key={link.route}>
+                                                <Link className="sidebar-link cursor-pointer" href={link.route}>
+                                                    <Image 
+                                                        src={link.icon}
+                                                        alt="logo"
+                                                        width={24}
+                                                        height={24}
+                                                    />
+                                                    {link.label}
+                                                </Link>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </>
+                        </SheetContent>
+                    </Sheet>
+                </SignedIn>
+                <SignedOut>
+                    <Button asChild className="button bg-purple-gradient bg-cover">
+                        <Link href="/sign-in">
+                            Sign In
+                        </Link>
+                    </Button>
+                </SignedOut>
+            </nav>
+        </header>
+    )
+}
+
+export default MobileNavbar
